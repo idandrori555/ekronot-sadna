cb3b84f91f7457432f0456c800b1bcb78bb91247
idan <idandrori555@gmail.com>
2026-02-02 16:16:45 +0200

    Initial commit added files

---
+++ idandrori/IndentationException.cpp
@@ -0,0 +1,6 @@
+#include "IndentationException.h"
+
+const char *IndentationException::what() const noexcept
+{
+  return "IndentationError: unexpected indent";
+}
---
+++ idandrori/IndentationException.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "InterpreterException.h"
+
+class IndentationException : public InterpreterException
+{
+public:
+  virtual const char *what() const noexcept;
+};
